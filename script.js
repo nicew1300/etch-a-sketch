9c48164b8e3a1197fae4d7e47ecd0dfebb84740d
Luka Stromajer <lukalel.kalulel@gmail.com>
2025-11-05 19:55:34 +0100

    Implement grid functionality with color options

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 let containerDiv = document.querySelector("#cont")
 
-for (i = 0; i < 32; i++) {
-    let div = document.createElement("div")
-    containerDiv.append(div)
+function black() {
+    let square = document.querySelectorAll(".square");
+    square.forEach(square => {
+        square.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+    });
+    })
 }
+
+function getRandomRGB() {
+    let threeRGB = `rgb(${Math.floor(Math.random() * (255 - 0 + 1)) + 0}, ${Math.floor(Math.random() * (255 - 0 + 1)) + 0}, ${Math.floor(Math.random() * (255 - 0 + 1)) + 0})`
+    return threeRGB
+}
+
+function rainbow() {
+    let square = document.querySelectorAll(".square");
+    square.forEach(square => {
+        square.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = getRandomRGB();
+    });
+    })
+}
+
+function getGridSize() {
+    let size = parseFloat(prompt("Number of squares per side for the grid? (number between 0 and 100)"))
+    while(size === 100 || size === 0 || size < 0 || size > 100 || isNaN(size) == true){
+        alert("Enter a valid number between 0 and 100! No more, no less.")
+        size = parseFloat(prompt("Number of squares per side for the grid? (number between 0 and 100)"))
+    }
+    return size
+}
+
+function makeGrid() {
+    containerDiv.innerHTML = ""
+    let size = getGridSize()
+    for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div")
+    square.classList.add("square")
+    containerDiv.appendChild(square)
+}
+}
+
+
+// assign class on buttons
+let buttons = Array.from(document.querySelectorAll("button"))
+buttons.forEach(btn => btn.classList.add("button"))
